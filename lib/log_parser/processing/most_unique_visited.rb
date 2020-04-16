fc2e8b29b46dfe703bc8577ad22edc4b42af1aa2
Roman Kugot <Kugot-ext@fidor.com>
2020-04-16 13:38:09 +0300

    Implement data processors

--- lib/log_parser/processing/most_unique_visited.rb
+++ lib/log_parser/processing/most_unique_visited.rb
@@ -3,6 +3,14 @@
 module LogParser
   module Processing
     class MostUniqueVisited
+      def self.perform(data)
+        grouped = data.group_by(&:url)
+        grouped.map { |url, ips| [url, ips.uniq(&:ip).size] }
+      end
+
+      def self.description
+        'List of webpages with most unique page views'
+      end
     end
   end
 end
--- lib/log_parser/processing/most_visited.rb
+++ lib/log_parser/processing/most_visited.rb
@@ -3,6 +3,14 @@
 module LogParser
   module Processing
     class MostVisited
+      def self.perform(data)
+        grouped = data.group_by(&:url)
+        grouped.map { |url, ips| [url, ips.size] }
+      end
+
+      def self.description
+        'List of webpages with most page views'
+      end
     end
   end
 end
