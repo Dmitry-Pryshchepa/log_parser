a49d041f18a92f858e334b4ec9dab21013575038
Roman Kugot <Kugot-ext@fidor.com>
2020-04-17 12:10:11 +0300

    Get rid of object state testing

--- spec/log_parser/executor_spec.rb
+++ spec/log_parser/executor_spec.rb
@@ -1,20 +1,23 @@
 # frozen_string_literal: true
 
 RSpec.describe LogParser::Executor do
-  subject { described_class.new(file) }
+  subject { described_class.new(file).perform }
 
   let(:file) { File.join('spec', 'fixtures', 'webserver.log') }
-
-  describe '#initialize' do
-    it { expect(subject.instance_variable_get(:@source)).to eq(file) }
-  end
+  let(:correct_output) { File.join('spec', 'fixtures', 'correct_script_output') }
+  let(:number_of_calls) { LogParser::Executor::PROCESSORS.count }
 
   describe '#perform' do
-    it 'proceeds parsing' do
-      expect(LogParser::Printer).to receive(:perform).twice
-      expect(LogParser::Processing::Sorter).to receive(:perform).twice
-      expect(LogParser::Processing::Decorator).to receive(:perform).twice
-      subject.perform
+    it 'proceeds parsing by the number of processors' do
+      expect(LogParser::Printer).to receive(:perform).exactly(number_of_calls).times
+      expect(LogParser::Processing::Sorter).to receive(:perform).exactly(number_of_calls).times
+      expect(LogParser::Processing::Decorator).to receive(:perform).exactly(number_of_calls).times
+
+      subject
+    end
+
+    it 'provides valid output' do
+      expect { subject }.to output(File.read(correct_output)).to_stdout
     end
   end
 end
--- spec/log_parser/log_line_spec.rb
+++ spec/log_parser/log_line_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe LogParser::LogLine do
 
   describe '#initialize' do
     it 'has a valid url attribute' do
-      expect(subject.instance_variable_get(:@url)).to eq(url)
+      expect(subject.url).to eq(url)
     end
 
     it 'has a valid ip attribute' do
-      expect(subject.instance_variable_get(:@ip)).to eq(ip)
+      expect(subject.ip).to eq(ip)
     end
   end
 end
